34f3dfd2297b265dabbdfc32dff47eff60112345
nonesubham <myselfsubhamsaurav@gmail.com>
2023-12-16 22:06:52 +0530

    Audio Player Almost

--- src/components/right_sidebar.vue
+++ src/components/right_sidebar.vue
@@ -1,47 +1,63 @@
 <template>
-
-    <div class="sideplayer-container">
-        <div class="sideplayer-flex-container">
-            <div class="sideplayer-item sideplayer-item-left">
-                <div class="centered-content">
-                    <i-mingcute-user-4-fill class="user-icon" />
-                    <span><b style="color:white"> Anonymous</b> <br><router-link to="/" style="text-decoration: none;"><span class="my-account-link" id="my_account">My Account</span></router-link></span>
-                </div>
-            </div>
-            <div class="sideplayer-item sideplayer-item-right">
-                <router-link to="/" style="text-decoration: none;">  <i-solar-bell-broken  width="25" height="25" color="white" id="alert_bell" /></router-link>
-            </div>
+  <div class="sideplayer-container">
+    <div class="sideplayer-flex-container">
+      <div class="sideplayer-item sideplayer-item-left">
+        <div class="centered-content">
+          <i-mingcute-user-4-fill class="user-icon" />
+          <span
+            ><b style="color: white"> Anonymous</b> <br /><router-link
+              to="/"
+              style="text-decoration: none"
+              ><span class="my-account-link" id="my_account"
+                >My Account</span
+              ></router-link
+            ></span
+          >
         </div>
-        <!-- Audio Player Start From Here -->
-        <div id="main_player">
-        <img :src="mus_thmb" id="mus_thmb">
-        <div class="flex-row">
-      <div class="box-20">
-        <i-fluent-speaker-mute-32-filled v-if="isMuted"  class="p-btn"/>
-        <i-fluent-speaker-28-filled v-else class="p-btn"/>
       </div>
-      <div class="box-60">
- 
-        <i-fluent-previous-32-filled  class="p-btn"/>
-        
-      <i-fluent-pause-32-filled v-if="isPlaying"  class="p-btn middle-btn"/>
-      <i-fluent-play-32-filled v-else class="p-btn middle-btn"/>
-    
-      <i-fluent-next-32-filled  class="p-btn"/>
-    
-      </div>
-      <div class="box-20">
-        <i-solar-repeat-one-minimalistic-bold v-if="isLooping" class="p-btn"/>
-        <i-solar-repeat-linear v-else class="p-btn"/>
+      <div class="sideplayer-item sideplayer-item-right">
+        <router-link to="/" style="text-decoration: none">
+          <i-solar-bell-broken width="25" height="25" color="white" id="alert_bell"
+        /></router-link>
       </div>
     </div>
-        <h2>Hello</h2>
-        </div>
-       </div>
-</template>
-  <script setup>
+    <!-- Audio Player Start From Here -->
+    <div id="mainPlayer">
+      <img :src="mus_thmb" :alt="mus_title" id="mus_thmb" />
+      <div id="mainCtrl">
 
-import { ref, onMounted, onUnmounted} from "vue";
+      <!-- mute -->
+        <div class="ctrlBtn muteBtn">
+          <i-solar-repeat-one-minimalistic-bold v-if="isLooping" class="cBtn"/>
+          <i-solar-repeat-bold v-else class="cBtn"/>
+          <!-- <i-solar-repeat-one-outline /> -->
+        </div>
+        <!-- previous -->
+        <div class="ctrlBtn" id="preBtn">
+          <i-line-md-arrow-close-left class="cBtn"/>
+          </div>
+          <!-- play/pause -->
+          <div id="playbtn" class="ctrlBtn">
+          <i-line-md-play-filled-to-pause-transition v-if="isPlaying" class="cBtn"/>
+        
+          <i-line-md-play-filled v-else class="cBtn"/>
+          
+        </div>
+        <!-- next -->
+        <div class="ctrlBtn" id="nxtBtn">
+          <i-line-md-arrow-close-right class="cBtn"/>
+        </div>
+        <!-- loop -->
+        <div class="ctrlBtn loopBtn">
+          <i-solar-volume-cross-bold v-if="isMuted" class="cBtn"/>
+          <i-solar-volume-loud-bold-duotone v-else class="cBtn"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
 //player config
 const audioPlayer = ref(null);
 const isPlaying = ref(false);
@@ -52,9 +68,8 @@ const isMuted = ref(false); // Track mute state
 const volume = ref(1);
 
 // api config
-const mus_title = ref("temp")
-const mus_thmb = ref("https://inv.in.projectsegfau.lt/vi/V_jp5_VAzXk/mqdefault.jpg")
-
+const mus_title = ref("temp");
+const mus_thmb = ref("https://img.youtube.com/vi/V_jp5_VAzXk/maxresdefault.jpg");
 
 const audioSource = "src/assets/sample2.mp3";
 
@@ -128,107 +143,93 @@ onUnmounted(() => {
 </script>
 <style scoped>
 .sideplayer-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
 
 .sideplayer-flex-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-}
-
-#alert_bell:hover {
-    color: #afafaf !important;
-}
-
-.sideplayer-item-left {
-    text-align: left;
-}
-
-.sideplayer-item-right {
-    text-align: right;
-}
-
-.centered-content {
-    display: flex;
-    align-items: center;
-}
-
-.user-icon {
-    font-size: 40px;
-    margin-right: 10px;
-    color: #afafaf;
-}
-
-.my-account-link {
-    color: #afafaf;
-    font-size: 13px;
-}
-
-.my-account-link:hover {
-    color: white;
-    font-weight: bold;
-}
-
-#main_player {
-    text-align: center; /* Center the text horizontally */
-}
-
-#main_player img {
-    max-width: 100%; 
-    border-radius: 30px;
-}
-.flex-row {
   display: flex;
-  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
 }
 
-.box-20,
-.box-60 {
-  box-sizing: border-box;
-  padding: 10px;
-  text-align: center;
+#alert_bell:hover {
+  color: #afafaf !important;
 }
 
-.box-20 {
-  flex: 20%;
-
+.sideplayer-item-left {
+  text-align: left;
 }
 
-.box-60 {
-  flex: 60%;
-
+.sideplayer-item-right {
+  text-align: right;
 }
 
-.p-btn{
+.centered-content {
+  display: flex;
+  align-items: center;
+}
+
+.user-icon {
+  font-size: 40px;
+  margin-right: 10px;
+  color: #afafaf;
+}
+
+.my-account-link {
+  color: #afafaf;
+  font-size: 13px;
+}
+
+.my-account-link:hover {
   color: white;
-  font-size: 18px;
+  font-weight: bold;
+}
+#mus_thmb {
+  height: 350px;
+  margin-top: 50px;
 }
 
-@keyframes scaleUp {
-      0% {
-        transform: scale(1);
-      }
-      100% {
-        transform: scale(0.9);
-      }
-    }
-.p-btn:hover{
-  color: rgba(255, 255, 255, 0.705);
-  font-size: 18px;
-  transition: transform 0.3s ease-in-out;
-  animation: scaleUp 0.3s forwards;
-
+#mainPlayer {
+  position: relative;
+  border-width: 2px;
 }
 
-.middle-btn{
-  margin: 0px 20%;
+#mainCtrl {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 50%;
+  bottom: 20px;
+  transform: translateX(-50%);
+  z-index: 100;
+  padding: 20px;
+  border-radius: 30px;
+  background-color: #3d3d3d88;
+  backdrop-filter: blur(5px);
+  border-width: 5px !important;
+  text-align: center;
+  width: 46%;
 }
+
+
+.cBtn:hover{
+color: #afafaf;
+}
+.ctrlBtn {
+  font-size: 20px;
+  color: white;
+}
+.loopBtn{
+  padding-left: 20px;
+}
+.muteBtn{
+  padding-right: 20px;
+}
+
 </style>
